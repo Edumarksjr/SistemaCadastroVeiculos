af444f84bc017011cd7db28a3b16bfb3104bf4ff
Eduardo <edumarksjr@hotmail.com>
2023-07-17 03:22:19 -0300

    Front

---
+++ SistemaCadastroVeiculos/Controllers/VeiculoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaCadastroVeiculos.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Visualizar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+    }
+
+}
